d366661897f52fc4bb17335fafd4568544a4c6e4
dawidplesniarski <dawidplesniarski@gmail.com>
2019-12-27 22:45:37 +0100

    code cleanup

--- src/main/java/spring/model/Member.java
+++ src/main/java/spring/model/Member.java
@@ -3,15 +3,15 @@ package spring.model;
 public class Member {
     private String firstName;
     private String lastName;
-    public Member() {
-    }
-    public Member(String firstName, String lastName) { this.firstName = firstName;
+    public Member() {}
+    public Member(String firstName, String lastName) {
+        this.firstName = firstName;
         this.lastName = lastName;
     }
     public String getFirstName() {
         return firstName;
     }
-    // pomininęte pozostałe settery i gettery
+
     public String toString() {
         return firstName + " " + lastName;
     }
