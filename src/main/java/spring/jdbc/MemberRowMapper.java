1045213d10c6a3495454375bc3230b87d63ebab3
dawidplesniarski <dawidplesniarski@gmail.com>
2020-01-17 21:57:34 +0100

    code cleanup

--- src/main/java/spring/jdbc/MemberRowMapper.java
+++ src/main/java/spring/jdbc/MemberRowMapper.java
@@ -13,7 +13,6 @@ public class MemberRowMapper implements RowMapper<Member> {
     @Override
     public Member mapRow(ResultSet resultSet, int rowNum) throws SQLException{
         Member member = new Member(resultSet.getString(1),resultSet.getString(2));
-
         return member;
     }
 }
